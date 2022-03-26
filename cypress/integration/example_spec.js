261e829e959a97a1df0141961f837f892b6e0c3c
José Yuri Lima Lira <yurilima.ce@gmail.com>
2022-03-26 12:00:54 -0300

    clear repository

--- cypress/integration/example_spec.js
+++
@@ -1,7 +0,0 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/login');
-    cy.get('[data-cy=submit]').type('fake@email.com');
-    cy.get('[data-cy=password]').type('123456');
-  });
-});
--- src/services/configApi.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const baseApi = axios.create({
-  baseURL: 'http://localhost:3004'
-});
-
-export default baseApi;
