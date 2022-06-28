ec98149719499c35c17dbab432b38d80e50333bb
Jose Yuri <yurilima.ce@gmail.com>
2022-06-28 18:50:55 -0300

    feat: add OPEN API swagger configuration

--- backend/collection-api/src/main.ts
+++ backend/collection-api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Manga Collection API')
+    .setDescription('Manga Collection api routes')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
